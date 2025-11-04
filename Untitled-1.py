785aeee2cc81adb9e006ebe7fbd062a7178ef29e
gabii <gabrielly.vilant.lima@escola.pr.gov.br>
2025-11-04 07:52:37 -0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,31 +1,25 @@
-print("**Mini Sistema de Usuário**/n")
-
+print("** MINI SISTEMA DE USUÁRIO **\n")
 
 def cadastro_new_user():
-    print("CADASTRO DE NOVOS USUÁRIOS :/n")
-    new_username = input ("Digite seu username")
-    new_password = input ("Digite sua nova senha: ")
-    nova_senha = new_password
-    new_password = input ("Confirme sua senha: ")
-    while (new_password != nova_senha):
-        new_password = input("Tente novamente! Digite sua nova senha: ") 
-    print("Olá, ", new_username,  ". Você foi cadastrado!")
+    print("CADASTRO DE NOVOS USUÁRIOS: \n")
+    new_username = input("Digite seu username: ")
+    new_password = input("Olá", new_username, "!Digite sua nova senha: ")
 
+    nova_senha = input(new_username, ", Confirme sua senha: ")
 
-opcao_inicial = int(input("Digite 1 para fazer LOGIN e digite 2 para CADASTRO: "))
-if opcao_inicial == 1 or opcao_inicial ==2:
-    if opcao_inicial == 1 :
-        nome_cad = "gabi"
-        senha_cad = "1414"
+    while new_password != nova_senha:
+     new_password = input("Tente novamente! Digite sua nova senha: ")
+    print("Olá, ", new_username, ". Você foi cadastrado!")
 
-        username_cad = input("Digite seu usuario cadastrado: ")
-        password_cad = input("Digite sua senha cadastrada: ")
+def definir_opcao(){
+opicao_inical = int(input("Digite 1 para fazer LOGIN ou Digite 2 para CADASTRO: \n"))
+if opcao_inicial == 1 or opcao_inicial == 2: # type: ignore
+    if opcao_inicial == 1:
+    user_cadastrado()
 
-        username_cad == nome_cad:
-        while password_cad != senha_cad
-        password_cad = input("Tente novamente! Digite sua senha cadastrada: ")
-        print("Usuário Logado no MINI SYSTEM")
     else:
-        cadastro_new_user()        
+        cadastro_new_user()
 else:
-   print("Opção não encontrada! Tente novamente mais tarde.")
+    print("Opção não encontrada! Tente novamente mais tarde.")
+}
+
